7b30d5ef9fca62bad0e2e4570b13c89bf0ed9c9c
oliosinter <olio.sinter@gmail.com>
2017-12-09 19:27:46 +0300

    started memo refactoring

--- internal/memo.go
+++ internal/memo.go
@@ -1,9 +1,9 @@
 package internal
 
-// Func is the type of the function to memoize.
-type Func func(key string) (interface{}, error)
+// MemoCallback is the type of the function to memoize.
+type MemoCallback func() (interface{}, error)
 
-// A result is the result of calling a Func.
+// A result is the result of calling a MemoCallback.
 type result struct {
 	value interface{}
 	err   error
@@ -14,9 +14,10 @@ type entry struct {
 	ready chan struct{} // closed when res is ready
 }
 
-// A request is a message requesting that the Func be applied to key.
+// A request is a message requesting that the MemoCallback be applied to key.
 type request struct {
 	key      string
+	callback MemoCallback
 	response chan<- result // the client wants a single result
 }
 
@@ -24,46 +25,52 @@ type Memo struct {
 	requests chan request
 }
 
-// New returns a memoization of f.  Clients must subsequently call Close.
-func New(f Func) *Memo {
-	memo := &Memo{requests: make(chan request)}
-	go memo.server(f)
+// New returns a memoization of callback.  Clients must subsequently call Close.
+func New(f MemoCallback) *Memo {
+	memo := &Memo{
+		requests: make(chan request),
+	}
+	go memo.server()
 	return memo
 }
 
-func (memo *Memo) Get(key string) (interface{}, error) {
+func (memo *Memo) Get(key string, callback MemoCallback) (interface{}, error) {
 	response := make(chan result)
-	memo.requests <- request{key, response}
+	memo.requests <- request{
+		key:      key,
+		callback: callback,
+		response: response,
+	}
 	res := <-response
+
 	return res.value, res.err
 }
 
 func (memo *Memo) Close() { close(memo.requests) }
 
-func (memo *Memo) server(f Func) {
+func (memo *Memo) server() {
 	cache := make(map[string]*entry)
 	for req := range memo.requests {
 		e := cache[req.key]
 		if e == nil {
 			// This is the first request for this key.
-			e = &entry{ready: make(chan struct{})}
+			e = &entry{
+				ready: make(chan struct{}),
+			}
 			cache[req.key] = e
-			go e.call(f, req.key) // call f(key)
+
+			go e.call(req.callback) // call callback(key)
 		}
 		go e.deliver(req.response)
 	}
 }
 
-func (e *entry) call(f Func, key string) {
-	// Evaluate the function.
-	e.res.value, e.res.err = f(key)
-	// Broadcast the ready condition.
+func (e *entry) call(f MemoCallback) {
+	e.res.value, e.res.err = f()
 	close(e.ready)
 }
 
 func (e *entry) deliver(response chan<- result) {
-	// Wait for the ready condition.
 	<-e.ready
-	// Send the result to the client.
 	response <- e.res
 }
