a4cc28a7df1c31420ecba6a118032001823ab2d8
oliosinter <olio.sinter@gmail.com>
2017-12-19 19:03:27 +0300

    fixed taxon struct

--- internal/sylius/api.go
+++ internal/sylius/api.go
@@ -8,24 +8,24 @@ import (
 
 // GetTaxon gets a category by code.
 func (c *Client) GetTaxon(ctx context.Context, code string) (*transfer.Taxon, error) {
-	result := &transfer.TaxonRaw{}
+	result := &transfer.Taxon{}
 	err := c.requestGet(ctx, c.getURL("/v1/taxons/%s", code), result)
 	if err != nil {
 		return nil, err
 	}
 
-	return transfer.ConvertRawTaxon(result), nil
+	return result, nil
 }
 
 // CreateTaxon creates a taxon
 func (c *Client) CreateTaxon(ctx context.Context, body transfer.TaxonNew) (*transfer.Taxon, error) {
-	result := &transfer.TaxonRaw{}
+	result := &transfer.Taxon{}
 	err := c.requestPost(ctx, c.getURL("/v1/taxons/"), result, body)
 	if err != nil {
 		return nil, err
 	}
 
-	return transfer.ConvertRawTaxon(result), nil
+	return result, nil
 }
 
 // GetProduct gets a product by code
--- internal/sylius/transfer/taxon.go
+++ internal/sylius/transfer/taxon.go
@@ -5,6 +5,8 @@ import (
 )
 
 // Taxon is a representation of a category in Sylius.
+// IMPORTANT!!! If you add new fields here, you have to add the same fields
+// to taxonRaw struct and extend UnmarshalJSON func (t *Taxon) UnmarshalJSON method.
 type Taxon struct {
 	ID           int                    `json:"id,omitempty"`
 	Code         string                 `json:"code,omitempty"`
@@ -14,41 +16,43 @@ type Taxon struct {
 	Translations map[string]Translation `json:"translations,omitempty"`
 }
 
+// A full copy of Taxon struct but has different Translations field definition.
+// This struct helps to unmarshal json to Taxon. Sylius api returns different
+// representation for Translations: if there are no translations, Sylius returns an
+// empty array, if not empty - an object.
+type taxonRaw struct {
+	ID           int             `json:"id,omitempty"`
+	Code         string          `json:"code,omitempty"`
+	Name         string          `json:"name,omitempty"`
+	Parent       *Taxon          `json:"parent,omitempty"`
+	Root         *Taxon          `json:"root,omitempty"`
+	Translations json.RawMessage `json:"translations"`
+}
+
+// UnmarshalJSON helps to fix inconsistency in sylius api response.
+func (t *Taxon) UnmarshalJSON(value []byte) error {
+	raw := &taxonRaw{}
+	if err := json.Unmarshal(value, raw); err != nil {
+		return err
+	}
+
+	t.ID = raw.ID
+	t.Code = raw.Code
+	t.Name = raw.Name
+	t.Parent = raw.Parent
+	t.Root = raw.Root
+
+	var tr map[string]Translation
+	if err := json.Unmarshal(raw.Translations, &tr); err == nil {
+		t.Translations = tr
+	}
+
+	return nil
+}
+
 // TaxonNew is a structure to be used in new taxon request.
 type TaxonNew struct {
 	Code         string                 `json:"code"`
 	Parent       string                 `json:"parent,omitempty"`
 	Translations map[string]Translation `json:"translations"`
-}
-
-// TaxonRaw is a helper to parse Sylius taxon.
-// Sylius api returns different representation for Translations:
-// if there are no translations, Sylius returns an empty array,
-// if not empty - an object.
-// TODO check if it is still actual information
-type TaxonRaw struct {
-	Taxon
-	Parent       *TaxonRaw       `json:"parent"`
-	Root         *TaxonRaw       `json:"root"`
-	Translations json.RawMessage `json:"translations"`
-}
-
-// ConvertRawTaxon converts TaxonRaw to Taxon type.
-func ConvertRawTaxon(raw *TaxonRaw) *Taxon {
-	taxon := raw.Taxon
-
-	if raw.Root != nil {
-		taxon.Root = ConvertRawTaxon(raw.Root)
-	}
-
-	if raw.Parent != nil {
-		taxon.Parent = ConvertRawTaxon(raw.Parent)
-	}
-
-	var t map[string]Translation
-	if err := json.Unmarshal(raw.Translations, &t); err == nil {
-		taxon.Translations = t
-	}
-
-	return &taxon
 }
