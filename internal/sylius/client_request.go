9101abb2a392f4d44578b67a1ad5fd2d01b88bb0
oliosinter <olio.sinter@gmail.com>
2017-12-21 15:59:43 +0300

    sylius client refactoring

--- internal/sylius/client_request.go
+++ internal/sylius/client_request.go
@@ -23,59 +23,73 @@ func (c *Client) requestPatch(ctx context.Context, url string, body interface{})
 	return c.request(ctx, methodPatch, url, nil, body)
 }
 
-// Performs a request. Sets authorization token and handles errors.
-// Creates context with timeout.
-// TODO this method seems to be long, should consider to split it.
-func (c *Client) request(ctx context.Context, method string, url string, result interface{}, body interface{}) error {
-	ctx, cancel := context.WithTimeout(ctx, c.requestTimeout)
-	defer cancel()
-
-	var err error
-
+// TODO
+func (c *Client) getRequestWithToken(ctx context.Context) (*resty.Request, error) {
 	token, err := c.getToken()
 	if err != nil {
-		c.logger.Errorf("Failed to get token during [%s] %s request", method, url)
-
-		return err
+		return nil, err
 	}
 
 	request := resty.R().SetContext(ctx).SetHeader("Authorization", "Bearer "+token)
-	if result != nil {
-		request.SetResult(result)
-	}
-	if body != nil {
-		request.SetBody(body)
-	}
 
-	var res *resty.Response
+	return request, nil
+}
 
-	c.logger.Debugf("Performing [%s] request to %s", method, url)
-
+// TODO
+func (c *Client) executeRequestWithMethod(request *resty.Request, method string, url string) (*resty.Response, error) {
 	switch method {
 	case methodGet:
-		res, err = request.Get(url)
+		return request.Get(url)
 	case methodPost:
-		res, err = request.Post(url)
+		return request.Post(url)
 	case methodPatch:
-		res, err = request.Patch(url)
-	default:
-		err = fmt.Errorf("unknown method")
+		return request.Patch(url)
 	}
 
-	if err != nil {
-		c.logger.Errorf(err.Error())
+	return nil, fmt.Errorf("unknown method")
+}
 
-		return fmt.Errorf(err.Error())
-	}
-
-	switch res.StatusCode() {
+// TODO
+func (c *Client) checkResponseStatus(response *resty.Response) error {
+	switch response.StatusCode() {
 	case http.StatusOK, http.StatusCreated, http.StatusNoContent:
 		return nil
 	case http.StatusNotFound:
 		return ErrNotFound
 	}
 
-	c.logger.Errorf("[%s] %s %s %s", method, url, res.Status(), string(res.Body()))
+	c.logger.Errorf("[%s] %s %s %s", response.Request.Method, response.Request.URL, response.Status(), string(response.Body()))
 
-	return fmt.Errorf("%s", res.Status())
+	return fmt.Errorf("%s", response.Status())
+}
+
+// Performs a request. Sets authorization token and handles errors.
+// Creates context with timeout.
+func (c *Client) request(ctx context.Context, method string, url string, result interface{}, body interface{}) error {
+	ctx, cancel := context.WithTimeout(ctx, c.requestTimeout)
+	defer cancel()
+
+	request, err := c.getRequestWithToken(ctx)
+	if err != nil {
+		return err
+	}
+
+	if result != nil {
+		request.SetResult(result)
+	}
+
+	if body != nil {
+		request.SetBody(body)
+	}
+
+	c.logger.Debugf("Performing [%s] request to %s", method, url)
+
+	response, err := c.executeRequestWithMethod(request, method, url)
+	if err != nil {
+		c.logger.Errorf(err.Error())
+
+		return fmt.Errorf(err.Error())
+	}
+
+	return c.checkResponseStatus(response)
 }
