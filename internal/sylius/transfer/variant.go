6b76db8dbbee14cee4c7342c7c5c4a8539cd0f43
oliosinter <olio.sinter@gmail.com>
2018-02-14 19:17:01 +0300

    VariantsEqual function

--- internal/sylius/transfer/variant.go
+++ internal/sylius/transfer/variant.go
@@ -1,5 +1,7 @@
 package transfer
 
+import "reflect"
+
 // VariantEntire is a representation of a product variant in Sylius.
 type VariantEntire struct {
 	Code          string                  `json:"code,omitempty"`
@@ -10,4 +12,21 @@ type VariantEntire struct {
 // Variant is a structure to be used in new product variant request.
 type Variant struct {
 	VariantEntire
+}
+
+// VariantsEqual checks if variants are equal
+func VariantsEqual(e VariantEntire, v Variant) bool {
+	if e.Code != v.Code {
+		return false
+	}
+
+	if !reflect.DeepEqual(e.Translations, v.Translations) {
+		return false
+	}
+
+	if !reflect.DeepEqual(e.ChannelPrices, v.ChannelPrices) {
+		return false
+	}
+
+	return true
 }
---
+++ internal/sylius/transfer/variant_test.go
@@ -0,0 +1,130 @@
+package transfer
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVariantsEqual(t *testing.T) {
+	for i, test := range testVariantsEqualCases {
+		// arrange
+		filter := test.filter(Variant{VariantEntire: getVariantMock()})
+
+		// act
+		// assert
+		assert.Equal(t, test.equal, VariantsEqual(getVariantMock(), filter), fmt.Sprintf("Failed test with index %d", i))
+	}
+}
+
+var testVariantsEqualCases = []struct {
+	equal  bool
+	filter func(v Variant) Variant
+}{
+	{
+		equal: true,
+		filter: func(v Variant) Variant {
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			v.Code = ""
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			tr := v.Translations["en_US"]
+			tr.Name = ""
+			v.Translations["en_US"] = tr
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			tr := v.Translations["en_US"]
+			tr.Description = ""
+			v.Translations["en_US"] = tr
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			tr := v.Translations["en_US"]
+			tr.Slug = ""
+			v.Translations["en_US"] = tr
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			tr := v.Translations["en_US"]
+			tr.ShortDescription = ""
+			v.Translations["en_US"] = tr
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			tr := v.Translations["en_US"]
+			v.Translations["new"] = tr
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			v.Translations = map[string]Translation{
+				"en_US": v.Translations["en_US"],
+			}
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			pr := v.ChannelPrices["default"]
+			pr.Price = 100.1
+			v.ChannelPrices["default"] = pr
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			pr := v.ChannelPrices["default"]
+			v.ChannelPrices["new"] = pr
+			return v
+		},
+	},
+	{
+		filter: func(v Variant) Variant {
+			v.ChannelPrices = map[string]ChannelPrice{}
+			return v
+		},
+	},
+}
+
+func getVariantMock() VariantEntire {
+	return VariantEntire{
+		Code: "variant",
+		Translations: map[string]Translation{
+			"en_US": {
+				Name:             "name",
+				Description:      "description",
+				Slug:             "slug",
+				ShortDescription: "short description",
+			},
+			"ru_RU": {
+				Name:             "name 2",
+				Description:      "description 2",
+				Slug:             "slug 2",
+				ShortDescription: "short description 2",
+			},
+		},
+		ChannelPrices: map[string]ChannelPrice{
+			"default": {
+				Price: 100.,
+			},
+		},
+	}
+}
